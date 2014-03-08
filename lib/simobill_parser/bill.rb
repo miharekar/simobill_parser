e697e4a69b0eaabdae7846afcd28ddcd6dfc9edc
Miha Rekar <info@mr.si>
2014-03-08 10:58:20 +0100

    calculates records costs and durations by record type

--- lib/simobill_parser/bill.rb
+++ lib/simobill_parser/bill.rb
@@ -1,3 +1,4 @@
+require 'bigdecimal'
 require 'nokogiri'
 require_relative 'record'
 
@@ -11,9 +12,26 @@ module SimobillParser
       @records ||= parse_records
     end
 
+    def filter(type)
+      records.select{ |r| r.description == type }
+    end
+
+    def cost(type)
+      filter(type).inject(0){ |sum, r| sum + BigDecimal.new(r.cost) }.to_f
+    end
+
+    def duration(type)
+      s = filter(type).inject(0) { |sum, r| sum + seconds_from_duration(r.duration)}
+      Time.at(s).utc.strftime('%H:%M:%S')
+    end
+
     private
     def parse_records
       @xml.xpath("//Zapis").map{ |z| Record.new(z) }
+    end
+
+    def seconds_from_duration(duration)
+      Time.parse(duration).to_i - Time.parse('00:00:00').to_i 
     end
   end
 end
--- spec/simobill_parser/bill_spec.rb
+++ spec/simobill_parser/bill_spec.rb
@@ -9,6 +9,23 @@ module SimobillParser
     it 'parses records as enumerable of Record' do
       bill.records.must_be_kind_of Enumerable
       bill.records.first.must_be_instance_of Record
+      bill.records.count.must_equal 148
+    end
+
+    describe 'by type' do
+      it 'filters records by type' do
+        bill.filter('Prenos podatkov').must_be_kind_of Enumerable
+        bill.filter('SMS sporočilo').first.number.must_equal '041580XXX'
+      end
+
+      it 'calculates cost' do
+        bill.cost('SMS sporočilo').must_equal 0
+        bill.cost('Klic v drugo mobilno omr.').must_equal 0.5013
+      end
+
+      it 'calculates exact duration' do
+        bill.duration('Klic v drugo mobilno omr.').must_equal '14:18:12'
+      end
     end
   end
 end

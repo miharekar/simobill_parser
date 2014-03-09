79a7db57031b6bd7b524fbfe3ca1ba80d7b2e4af
Miha Rekar <info@mr.si>
2014-03-09 09:28:54 +0100

    gitignore only OS stuff

--- spec/simobill_parser/bill_spec.rb
+++ spec/simobill_parser/bill_spec.rb
@@ -3,8 +3,7 @@ require 'simobill_parser/bill'
 
 module SimobillParser
   describe Bill do
-    let(:sample) { File.read('spec/fixtures/sample.xml') }
-    let(:bill) { Bill.new(sample) }
+    let(:bill) { Bill.new(File.read('spec/fixtures/sample.xml')) }
 
     it 'parses records as enumerable of Record' do
       bill.records.must_be_kind_of Enumerable
